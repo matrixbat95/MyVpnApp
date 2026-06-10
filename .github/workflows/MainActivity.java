29906c33a74268e30597926490b02dabf18bc33e
matrixbat95 <151771584+matrixbat95@users.noreply.github.com>
2026-06-10 23:29:05 +0300

    Add files via upload

---
+++ .github/workflows/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("VPN Приложение работает!");
+        setContentView(tv);
+    }
+}
